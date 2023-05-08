5202e8ddc6e6d543c538e4773ac69ae779aa3f4a
Matthew Rinaldi <62622130+marinalid1@users.noreply.github.com>
2023-05-08 19:54:48 -0400

    Add files via upload

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,26 +8,36 @@ public class GuessingGame {
         Scanner input = new Scanner(System.in);
          // Create a Random object
         Random random = new Random();
-
          // Generate a random integer between 1 and 10
         int randomInt = random.nextInt(10) + 1;
         // chances variable keeps chance of how many attempts the user has left
         int chances = 3;
+        // creates HashSet to keep track of previous guesses
+        HashSet<Integer> set = new HashSet<>();
 
         // while chancres are greater than 0
         while (chances > 0) {
             // Ask user to enter integer between 1-10
             System.out.print("Enter an integer between 1-10: ");
             // number variable stores user int
-            int number = input.nextInt();
+            var number = input.nextInt();
             // if number and randomInt are equal user has won
             if (number == randomInt) {
                 System.out.println("You win!");
                 break;
-            } 
+            }
+            // checks if number is in proper range
+            else if (number < 1 || number > 10) {
+                System.out.println("Number must be between 1-10");
+            }
+            // checks if number has already beeen used
+            else if(set.contains(Integer.valueOf(number))){
+                System.out.println("Number already guessed");
+            }
             // if chances - 1 is greater than 0 subtract a chance and let  user know how many more they have
             else if (chances - 1 > 0) {
                 chances--;
+                set.add(number);
                 System.out.println("That is not it, please try again. You now have " + chances + " more chances left.");
             } 
             // else user has run out of chances and the losing message is displayed
